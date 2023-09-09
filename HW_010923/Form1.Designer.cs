dea3de6809860aff49024a74ccd665f0d9ea4d57
Velmeri <danielmikhh2gmail.com>
2023-09-09 15:46:44 +0200

    Добавьте файлы проекта.

---
+++ HW_010923/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace HW_010923
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			b7 = new Button();
+			b8 = new Button();
+			b9 = new Button();
+			b4 = new Button();
+			b5 = new Button();
+			b6 = new Button();
+			b1 = new Button();
+			b2 = new Button();
+			b3 = new Button();
+			b0 = new Button();
+			tbDisplay = new TextBox();
+			bСomma = new Button();
+			bPlus = new Button();
+			bMinus = new Button();
+			bMultiplication = new Button();
+			bDivision = new Button();
+			bEqual = new Button();
+			bLArrow = new Button();
+			bAC = new Button();
+			SuspendLayout();
+			// 
+			// b7
+			// 
+			b7.Location = new Point(12, 91);
+			b7.Name = "b7";
+			b7.Size = new Size(55, 54);
+			b7.TabIndex = 0;
+			b7.Text = "7";
+			b7.UseVisualStyleBackColor = true;
+			b7.Click += Updating_Task_Conditions;
+			// 
+			// b8
+			// 
+			b8.Location = new Point(73, 91);
+			b8.Name = "b8";
+			b8.Size = new Size(55, 54);
+			b8.TabIndex = 0;
+			b8.Text = "8";
+			b8.UseVisualStyleBackColor = true;
+			b8.Click += Updating_Task_Conditions;
+			// 
+			// b9
+			// 
+			b9.Location = new Point(134, 91);
+			b9.Name = "b9";
+			b9.Size = new Size(55, 54);
+			b9.TabIndex = 0;
+			b9.Text = "9";
+			b9.UseVisualStyleBackColor = true;
+			b9.Click += Updating_Task_Conditions;
+			// 
+			// b4
+			// 
+			b4.Location = new Point(12, 151);
+			b4.Name = "b4";
+			b4.Size = new Size(55, 54);
+			b4.TabIndex = 0;
+			b4.Text = "4";
+			b4.UseVisualStyleBackColor = true;
+			b4.Click += Updating_Task_Conditions;
+			// 
+			// b5
+			// 
+			b5.Location = new Point(73, 151);
+			b5.Name = "b5";
+			b5.Size = new Size(55, 54);
+			b5.TabIndex = 0;
+			b5.Text = "5";
+			b5.UseVisualStyleBackColor = true;
+			b5.Click += Updating_Task_Conditions;
+			// 
+			// b6
+			// 
+			b6.Location = new Point(134, 151);
+			b6.Name = "b6";
+			b6.Size = new Size(55, 54);
+			b6.TabIndex = 0;
+			b6.Text = "6";
+			b6.UseVisualStyleBackColor = true;
+			b6.Click += Updating_Task_Conditions;
+			// 
+			// b1
+			// 
+			b1.Location = new Point(12, 211);
+			b1.Name = "b1";
+			b1.Size = new Size(55, 54);
+			b1.TabIndex = 0;
+			b1.Text = "1";
+			b1.UseVisualStyleBackColor = true;
+			b1.Click += Updating_Task_Conditions;
+			// 
+			// b2
+			// 
+			b2.Location = new Point(73, 211);
+			b2.Name = "b2";
+			b2.Size = new Size(55, 54);
+			b2.TabIndex = 0;
+			b2.Text = "2";
+			b2.UseVisualStyleBackColor = true;
+			b2.Click += Updating_Task_Conditions;
+			// 
+			// b3
+			// 
+			b3.Location = new Point(134, 211);
+			b3.Name = "b3";
+			b3.Size = new Size(55, 54);
+			b3.TabIndex = 0;
+			b3.Text = "3";
+			b3.UseVisualStyleBackColor = true;
+			b3.Click += Updating_Task_Conditions;
+			// 
+			// b0
+			// 
+			b0.Location = new Point(12, 271);
+			b0.Name = "b0";
+			b0.Size = new Size(116, 54);
+			b0.TabIndex = 0;
+			b0.Text = "0";
+			b0.UseVisualStyleBackColor = true;
+			b0.Click += Updating_Task_Conditions;
+			// 
+			// tbDisplay
+			// 
+			tbDisplay.Location = new Point(12, 20);
+			tbDisplay.Multiline = true;
+			tbDisplay.Name = "tbDisplay";
+			tbDisplay.ReadOnly = true;
+			tbDisplay.Size = new Size(238, 65);
+			tbDisplay.TabIndex = 1;
+			// 
+			// bСomma
+			// 
+			bСomma.Location = new Point(134, 271);
+			bСomma.Name = "bСomma";
+			bСomma.Size = new Size(55, 54);
+			bСomma.TabIndex = 0;
+			bСomma.Text = ".";
+			bСomma.UseVisualStyleBackColor = true;
+			bСomma.Click += Updating_Task_Conditions;
+			// 
+			// bPlus
+			// 
+			bPlus.Location = new Point(195, 271);
+			bPlus.Name = "bPlus";
+			bPlus.Size = new Size(55, 54);
+			bPlus.TabIndex = 0;
+			bPlus.Text = "+";
+			bPlus.UseVisualStyleBackColor = true;
+			bPlus.Click += Updating_Task_Conditions;
+			// 
+			// bMinus
+			// 
+			bMinus.Location = new Point(195, 211);
+			bMinus.Name = "bMinus";
+			bMinus.Size = new Size(55, 54);
+			bMinus.TabIndex = 0;
+			bMinus.Text = "-";
+			bMinus.UseVisualStyleBackColor = true;
+			bMinus.Click += Updating_Task_Conditions;
+			// 
+			// bMultiplication
+			// 
+			bMultiplication.Location = new Point(195, 151);
+			bMultiplication.Name = "bMultiplication";
+			bMultiplication.Size = new Size(55, 54);
+			bMultiplication.TabIndex = 0;
+			bMultiplication.Text = "*";
+			bMultiplication.UseVisualStyleBackColor = true;
+			bMultiplication.Click += Updating_Task_Conditions;
+			// 
+			// bDivision
+			// 
+			bDivision.Location = new Point(195, 91);
+			bDivision.Name = "bDivision";
+			bDivision.Size = new Size(55, 54);
+			bDivision.TabIndex = 0;
+			bDivision.Text = "/";
+			bDivision.UseVisualStyleBackColor = true;
+			bDivision.Click += Updating_Task_Conditions;
+			// 
+			// bEqual
+			// 
+			bEqual.Location = new Point(134, 331);
+			bEqual.Name = "bEqual";
+			bEqual.Size = new Size(116, 54);
+			bEqual.TabIndex = 0;
+			bEqual.Text = "=";
+			bEqual.UseVisualStyleBackColor = true;
+			bEqual.Click += bEqual_Click;
+			// 
+			// bLArrow
+			// 
+			bLArrow.Location = new Point(12, 331);
+			bLArrow.Name = "bLArrow";
+			bLArrow.Size = new Size(55, 54);
+			bLArrow.TabIndex = 0;
+			bLArrow.Text = "←";
+			bLArrow.UseVisualStyleBackColor = true;
+			bLArrow.Click += bLArrow_Click;
+			// 
+			// bAC
+			// 
+			bAC.Location = new Point(73, 331);
+			bAC.Name = "bAC";
+			bAC.Size = new Size(55, 54);
+			bAC.TabIndex = 0;
+			bAC.Text = "AC";
+			bAC.UseVisualStyleBackColor = true;
+			bAC.Click += bAC_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(269, 404);
+			Controls.Add(tbDisplay);
+			Controls.Add(b3);
+			Controls.Add(b6);
+			Controls.Add(b2);
+			Controls.Add(b5);
+			Controls.Add(b9);
+			Controls.Add(bDivision);
+			Controls.Add(bMultiplication);
+			Controls.Add(bMinus);
+			Controls.Add(bPlus);
+			Controls.Add(bAC);
+			Controls.Add(bLArrow);
+			Controls.Add(bСomma);
+			Controls.Add(bEqual);
+			Controls.Add(b0);
+			Controls.Add(b1);
+			Controls.Add(b4);
+			Controls.Add(b8);
+			Controls.Add(b7);
+			Name = "Form1";
+			Text = "Calculator";
+			Load += Form1_Load;
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button b7;
+		private Button b8;
+		private Button b9;
+		private Button b4;
+		private Button b5;
+		private Button b6;
+		private Button b1;
+		private Button b2;
+		private Button b3;
+		private Button b0;
+		private TextBox tbDisplay;
+		private Button bСomma;
+		private Button bPlus;
+		private Button bMinus;
+		private Button bMultiplication;
+		private Button bDivision;
+		private Button bEqual;
+		private Button bLArrow;
+		private Button bAC;
+	}
+}
---
+++ HW_010923/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+
+namespace HW_010923
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		// Всё гениальное - просто. Это собыьтие срабатывает когда мы меняем мат формулу задачи. Мы просто переводим 'sender' в строку и достаем последний символ.
+		private void Updating_Task_Conditions(object sender, EventArgs e)
+		{
+			tbDisplay.Text += sender.ToString()[sender.ToString().Length - 1];
+		}
+
+		private void bEqual_Click(object sender, EventArgs e)
+		{
+			string MathFormula = tbDisplay.Text;
+
+			try {
+				DataTable table = new DataTable();
+				table.Columns.Add("expression", typeof(string), MathFormula);
+
+				DataRow row = table.NewRow();
+				table.Rows.Add(row);
+
+				double result = double.Parse((string)row["expression"]);
+				tbDisplay.Text = result.ToString();
+			} catch (Exception ex) {
+				tbDisplay.Text = ("Error: " + ex.Message);
+			}
+		}
+
+		private void bLArrow_Click(object sender, EventArgs e)
+		{
+			tbDisplay.Text = tbDisplay.Text.Substring(0, tbDisplay.Text.Length - 1);
+		}
+
+		private void bAC_Click(object sender, EventArgs e)
+		{
+			tbDisplay.Text = null;
+		}
+	}
+}
